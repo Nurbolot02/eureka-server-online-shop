87b0fc8f1d5ad48f7c1ffaa7544167013abec754
Nurbolot <ngulamidinov@gmail.com>
2023-02-21 18:03:18 +0600

    init commit

---
+++ src/main/java/com/ntg/eurekaserveronlineshop/EurekaServerOnlineShopApplication.java
@@ -0,0 +1,15 @@
+package com.ntg.eurekaserveronlineshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServerOnlineShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerOnlineShopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ntg/eurekaserveronlineshop/EurekaServerOnlineShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ntg.eurekaserveronlineshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerOnlineShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
