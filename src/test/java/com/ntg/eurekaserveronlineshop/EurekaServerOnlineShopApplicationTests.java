607949f80da0693c75d2d54ab03472270cd846bb
nurbolot <ngulamidinov@gmail.com>
2023-04-21 00:28:14 +0600

    add test

--- src/test/java/com/ntg/eurekaserveronlineshop/EurekaServerOnlineShopApplicationTests.java
+++ src/test/java/com/ntg/eurekaserveronlineshop/EurekaServerOnlineShopApplicationTests.java
@@ -1,5 +1,6 @@
 package com.ntg.eurekaserveronlineshop;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +9,7 @@ class EurekaServerOnlineShopApplicationTests {
 
     @Test
     void contextLoads() {
+        Assertions.assertTrue(true);
     }
 
 }
